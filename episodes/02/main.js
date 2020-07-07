aea0c677472f2105151f2770d0bd9475bad9f3ec
maxdevjs <max.devjs@gmail.com>
2020-07-07 05:00:56 -0300

    Update episode 02 graphic

--- episodes/02/main.js
+++ episodes/02/main.js
@@ -5,7 +5,9 @@ window.onload = function () {
         height = canvas.height = window.innerHeight;
 
   // context.fillRect(0, 0, width, height);
-
+  
+  context.font = "20px Arial";
+  
   context.beginPath();       // Start a new path
   context.moveTo(0, height / 2);    // Move the pen to (0, height / 2)
   context.lineTo(width, height / 2);  // Draw a line to (width, height / 2)
@@ -14,16 +16,20 @@ window.onload = function () {
   // CanvasRenderingContext2D.translate
   // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate
   context.translate(50, height / 2);
-  context.scale(1, -1);
+  // context.scale(1, -1);
   context.fillStyle = "red";
 
   for (let angle = 0; angle < Math.PI * 2; angle += .02) {
     let x = angle * 200,
         y = Math.sin(angle) * 200;
 
-    const degree = Math.floor((angle * 180) / Math.PI);
-    console.log(degree);
-    if (degree === 0 || degree === 90 || degree === 180 || degree === 270 || degree === 359) {
+    const degree = Math.ceil((angle * 180) / Math.PI);
+    // console.log(`${degree} - ${angle}`);
+    
+    if (degree === 0 || degree === 90 || degree === 180 || degree === 270 || degree === 360) {
+      context.fillText(degree + "°" + " (" + angle + " rad)", x, -(height / 3));
+      // context.fillText(angle + " rad", x, -(height / 3.5));
+
       context.beginPath();
       context.moveTo(x, height / 3); // top
       context.lineTo(x, -height / 3); // bottom
